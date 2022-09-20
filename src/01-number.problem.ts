bd9f048f414c90cca9d108735ee0a867a3ec941b
c0descrub <jordanjames1095@googlemail.com>
2022-09-20 20:51:21 +0100

    Day 1 progress

--- src/01-number.problem.ts
+++ src/01-number.problem.ts
@@ -1,7 +1,7 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (a, b) => {
-  return a + b;
+export const addTwoNumbers = (a:number , b:number) => {
+  return a + b
 };
 
 it("Should add the two numbers together", () => {
